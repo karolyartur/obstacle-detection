90e01f13613fb0542b7bfda117dcf81668af871a
Artur Istvan Karoly <artur.karoly@irob.uni-obuda.hu>
2020-09-22 19:17:55 +0200

    Update data_collection

--- data_collection_realsense.py
+++ data_collection_realsense.py
@@ -11,6 +11,10 @@ import cv2
 import realsense_input_module as ri
 import argparse
 import ur_tests_module as tests
+import Msg
+import zmq
+import flatbuffers
+import time
 
 
 ## Parse command line
@@ -43,29 +47,41 @@ def parse_args():
 
 def main(args):
 
-    while True:
+    with open('data.txt', 'w') as f:
+        while True:
 
-        # Wait for a coherent pair of frames: depth and color
-        frames_sr300 = ri.get_frames(pipeline_1)
-        depth_frame = ri.get_depth_frames(frames_sr300)
-        color_frame = ri.get_color_frames(frames_sr300)
-        if not depth_frame or not color_frame:
-            continue
+            msg_time = time.time()
+            if use_zmq_data:
+                array_buffer = socket.recv()
+                data_bytearray = bytearray(array_buffer)
+                msg = Msg.Msg.GetRootAsMsg(array_buffer,0)
+                msg_as_np = msg.ValueVectorAsNumpy()
+            else:
+                msg_as_np = np.array([0,0,0,0,0])
+            if args.record:
+                f.write(np.array_str(msg_as_np)+','+str(msg_time-start_time)+'\n')
 
-        # Convert images to numpy arrays
-        depth_image = ri.convert_img_to_nparray(depth_frame)
-        color_image = ri.convert_img_to_nparray(color_frame)
+            # Wait for a coherent pair of frames: depth and color
+            frames_sr300 = ri.get_frames(pipeline_1)
+            depth_frame = ri.get_depth_frames(frames_sr300)
+            color_frame = ri.get_color_frames(frames_sr300)
+            if not depth_frame or not color_frame:
+                continue
+
+            # Convert images to numpy arrays
+            depth_image = ri.convert_img_to_nparray(depth_frame)
+            color_image = ri.convert_img_to_nparray(color_frame)
 
 
-        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-        depth_colormap = ri.get_depth_colormap(depth_image, 0.03)
+            # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+            depth_colormap = ri.get_depth_colormap(depth_image, 0.03)
 
-        # Stack both images horizontally
-        images = np.hstack((color_image, depth_colormap))
+            # Stack both images horizontally
+            images = np.hstack((color_image, depth_colormap))
 
-        # Show images
-        if args.minimal:
-            ri.show_imgs('SR300', images, 1)
+            # Show images
+            if args.minimal:
+                ri.show_imgs('SR300', images, 1)
 
 if __name__ == "__main__":
 
@@ -89,9 +105,24 @@ if __name__ == "__main__":
     if args.playback:
         profile_2 = tests.start_sr300_2(config_1, pipeline_1, 640, 480, 30, 'data/' + args.filename +'.bag')
 
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    use_zmq_data = True
+    socket.connect("tcp://localhost:1111")
+    socket.subscribe(b'd\x02\x00\x03')
+    try:
+        print(socket.recv(zmq.NOBLOCK))
+        socket.recv()
+        print('Received data from ZMQ')
+        print("\nConnected to ZMQ\n")
+    except:
+        use_zmq_data = False
+        print('\nUnable to get data from ZMQ!')
+
     if args.record:
         config_1.enable_record_to_file('data/' + args.filename + '.bag')
 
+    start_time = time.time()
     # Start pipeline
     if not args.playback:
         pipeline_1.start(config_1)
